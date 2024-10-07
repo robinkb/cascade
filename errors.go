8133f8a77d9975591ee5a295dee857aae79e0df8
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2024-10-07 19:07:15 +0200

    Encode errors from the Distribution spec

---
+++ errors.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"strings"
+)
+
+var (
+	// Error codes as defined in the Distribution specification:
+	// https://github.com/opencontainers/distribution-spec/blob/main/spec.md#error-codes
+	ErrBlobUnknown         = Error{Code: "BLOB_UNKNOWN", Message: "blob unknown to registry"}
+	ErrBlobUploadInvalid   = Error{Code: "BLOB_UPLOAD_INVALID", Message: "blob upload invalid"}
+	ErrBlobUploadUnknown   = Error{Code: "BLOB_UPLOAD_UNKNOWN", Message: "blob upload unknown to registry"}
+	ErrDigestInvalid       = Error{Code: "DIGEST_INVALID", Message: "provided digest did not match uploaded content"}
+	ErrManifestBlobUnknown = Error{Code: "MANIFEST_BLOB_UNKNOWN", Message: "manifest references a manifest or blob unknown to registry"}
+	ErrManifestInvalid     = Error{Code: "MANIFEST_INVALID", Message: "manifest invalid"}
+	ErrManifestUnknown     = Error{Code: "MANIFEST_UNKNOWN", Message: "manifest unknown to registry"}
+	ErrNameInvalid         = Error{Code: "NAME_INVALID", Message: "invalid repository name"}
+	ErrNameUnknown         = Error{Code: "NAME_UNKNOWN", Message: "repository name not known to registry"}
+	ErrSizeInvalid         = Error{Code: "SIZE_INVALID", Message: "provided length did not match content length"}
+	ErrUnauthorized        = Error{Code: "UNAUTHORIZED", Message: "authentication required"}
+	ErrDenied              = Error{Code: "DENIED", Message: "requested access to the resource is denied"}
+	ErrUnsupported         = Error{Code: "UNSUPPORTED", Message: "the operation is unsupported"}
+	ErrTooManyRequests     = Error{Code: "TOOMANYREQUESTS", Message: "too many requests"}
+)
+
+type ErrorList struct {
+	Errors []Error `json:"errors"`
+}
+
+func (e *ErrorList) Error() string {
+	errs := make([]string, len(e.Errors))
+	for i := range e.Errors {
+		errs[i] = e.Errors[i].Error()
+	}
+
+	return strings.Join(errs, ", ")
+}
+
+type Error struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Detail  string `json:"detail,omitempty"`
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
