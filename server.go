26a7484886773acf3640a970adc14483776108f7
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2024-10-16 20:35:12 +0200

    Refactor request routing code

--- server.go
+++ server.go
@@ -66,165 +66,198 @@ type RegistryServer struct {
 }
 
 func (s *RegistryServer) manifestsHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-	reference := r.PathValue("reference")
-
 	switch r.Method {
 	case http.MethodHead:
-		info, err := s.service.StatManifest(name, reference)
-		if err != nil {
-			mapError(w, err)
-			return
-		}
-
-		w.Header().Set(headerContentLength, strconv.Itoa(int(info.Size)))
-		w.WriteHeader(http.StatusOK)
-
+		s.statManifestsHandler(w, r)
 	case http.MethodGet:
-		// TODO: This is doing too much. GetManifest should verify the Manifest,
-		// and return the media type.
-		var manifest v1.Manifest
-		content, err := s.service.GetManifest(name, reference)
-		if err != nil {
-			mapError(w, err)
-			return
-		}
-		json.Unmarshal(content, &manifest)
-
-		w.Header().Set(headerContentType, manifest.MediaType)
-		w.WriteHeader(http.StatusOK)
-		w.Write(content)
-
+		s.getManifestsHandler(w, r)
 	case http.MethodPut:
-		// The stored manifest must be an exact byte representation.
-		data, err := io.ReadAll(r.Body)
-		if err != nil {
-			mapError(w, err)
-			return
-		}
-		err = s.service.PutManifest(name, reference, data)
-		if err != nil {
-			mapError(w, err)
-			return
-		}
-		w.WriteHeader(http.StatusCreated)
-
+		s.putManifestsHandler(w, r)
 	case http.MethodDelete:
 		w.WriteHeader(http.StatusAccepted)
-
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
 func (s *RegistryServer) blobsHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-	digest := r.PathValue("digest")
-
 	switch r.Method {
 	case http.MethodHead:
-		if _, err := s.service.StatBlob(name, digest); err != nil {
-			mapError(w, err)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-
+		s.statBlobsHandler(w, r)
 	case http.MethodGet:
-		content, err := s.service.GetBlob(digest)
-		if err != nil {
-			mapError(w, err)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Write(content)
-
+		s.getBlobsHandler(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
 func (s *RegistryServer) blobsUploadsSessionHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-
 	switch r.Method {
 	case http.MethodPost:
-		session := s.service.InitUpload(name)
-		w.Header().Set(headerLocation, session.Location)
-		w.WriteHeader(http.StatusAccepted)
-
+		s.initUploadHandler(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
 func (s *RegistryServer) blobsUploadsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPut:
+		s.closeUploadHandler(w, r)
+	case http.MethodPatch:
+		s.writeUploadHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func (s *RegistryServer) statManifestsHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.PathValue("name")
 	reference := r.PathValue("reference")
 
-	switch r.Method {
-	case http.MethodPut:
-		_, err := s.service.StatUpload(reference)
-		if err != nil {
-			mapError(w, err)
-			return
-		}
+	info, err := s.service.StatManifest(name, reference)
+	if err != nil {
+		mapError(w, err)
+		return
+	}
 
-		// This is either a monolithic upload, or closing a chunked upload
-		// with a final chunk.
-		if r.Body != nil {
-			// Content-Type and Content-Length should be set if the request
-			// contains a body.
-			if r.Header.Get(headerContentType) != contentTypeOctetStream ||
-				r.Header.Get(headerContentLength) == "" {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
+	w.Header().Set(headerContentLength, strconv.Itoa(int(info.Size)))
+	w.WriteHeader(http.StatusOK)
+}
 
-			content, _ := io.ReadAll(r.Body)
-			// TODO: Check this error
-			s.service.WriteUpload(reference, content)
-		}
+func (s *RegistryServer) getManifestsHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	reference := r.PathValue("reference")
 
-		digest := r.URL.Query().Get("digest")
-		if digest == "" {
+	// TODO: This is doing too much. GetManifest should verify the Manifest,
+	// and return the media type.
+	var manifest v1.Manifest
+	content, err := s.service.GetManifest(name, reference)
+	if err != nil {
+		mapError(w, err)
+		return
+	}
+	json.Unmarshal(content, &manifest)
+
+	w.Header().Set(headerContentType, manifest.MediaType)
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
+
+func (s *RegistryServer) putManifestsHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	reference := r.PathValue("reference")
+
+	// The stored manifest must be an exact byte representation.
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		mapError(w, err)
+		return
+	}
+	err = s.service.PutManifest(name, reference, data)
+	if err != nil {
+		mapError(w, err)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s *RegistryServer) statBlobsHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	digest := r.PathValue("digest")
+
+	if _, err := s.service.StatBlob(name, digest); err != nil {
+		mapError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (s *RegistryServer) getBlobsHandler(w http.ResponseWriter, r *http.Request) {
+	digest := r.PathValue("digest")
+
+	content, err := s.service.GetBlob(digest)
+	if err != nil {
+		mapError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
+
+func (s *RegistryServer) initUploadHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	session := s.service.InitUpload(name)
+	w.Header().Set(headerLocation, session.Location)
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (s *RegistryServer) writeUploadHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	reference := r.PathValue("reference")
+
+	content, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := s.service.WriteUpload(reference, content); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	location := fmt.Sprintf("/v2/%s/blobs/uploads/%s", name, reference)
+	w.Header().Set(headerLocation, location)
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (s *RegistryServer) closeUploadHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	reference := r.PathValue("reference")
+
+	_, err := s.service.StatUpload(reference)
+	if err != nil {
+		mapError(w, err)
+		return
+	}
+
+	// This is either a monolithic upload, or closing a chunked upload
+	// with a final chunk.
+	if r.Body != nil {
+		// Content-Type and Content-Length should be set if the request
+		// contains a body.
+		if r.Header.Get(headerContentType) != contentTypeOctetStream ||
+			r.Header.Get(headerContentLength) == "" {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		err = s.service.CloseUpload(reference, digest)
-		if err != nil {
-			if errors.Is(err, ErrDigestInvalid) {
-				w.WriteHeader(http.StatusBadRequest)
-			} else {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			return
-		}
-
-		location := fmt.Sprintf("/v2/%s/blobs/%s", name, digest)
-		w.Header().Set(headerLocation, location)
-		w.WriteHeader(http.StatusCreated)
-
-	case http.MethodPatch:
-		content, err := io.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if err := s.service.WriteUpload(reference, content); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		location := fmt.Sprintf("/v2/%s/blobs/uploads/%s", name, reference)
-		w.Header().Set(headerLocation, location)
-		w.WriteHeader(http.StatusAccepted)
-
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		content, _ := io.ReadAll(r.Body)
+		// TODO: Check this error
+		s.service.WriteUpload(reference, content)
 	}
+
+	digest := r.URL.Query().Get("digest")
+	if digest == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = s.service.CloseUpload(reference, digest)
+	if err != nil {
+		if errors.Is(err, ErrDigestInvalid) {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	location := fmt.Sprintf("/v2/%s/blobs/%s", name, digest)
+	w.Header().Set(headerLocation, location)
+	w.WriteHeader(http.StatusCreated)
 }
 
 // This is starting to feel like the wrong approach.
