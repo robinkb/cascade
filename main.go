9958efd1736963f31357afc401c5e4a0f3d1384e
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2024-10-21 20:20:46 +0200

    Add basic request logging

--- main.go
+++ main.go
@@ -9,5 +9,12 @@ func main() {
 	store := NewInMemoryStore()
 	service := NewRegistryService(store)
 	server := NewRegistryServer(service)
-	log.Fatal(http.ListenAndServe(":5000", server))
+	log.Fatal(http.ListenAndServe(":5000", logger(server)))
+}
+
+func logger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+		log.Printf("%s %s\n", r.Method, r.URL.Path)
+	})
 }
