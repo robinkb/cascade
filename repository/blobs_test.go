3d1e67095d59670ad7a0792233d4572b4c7d8780
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2025-05-25 13:14:53 +0200

    Refactor Repository tests into a suite (#38)
    * Start refactoring repository tests into a suite
    
    * Refactor blob tests
    
    * Refactor manifests and referrers
    
    * Refactor remaining repository tests
    
    * Add missing error checks
    
    * Small test tweaks

--- repository/blobs_test.go
+++ repository/blobs_test.go
@@ -8,82 +8,76 @@ import (
 	. "github.com/robinkb/cascade-registry/testing"
 )
 
-func TestStatBlob(t *testing.T) {
-	service, metadata, blobs := newTestRepository()
-
+func (s *Suite) TestStatBlob() {
 	name := RandomName()
 	digest, content := RandomBlob(32 * 1024)
 
-	err := blobs.PutBlob(digest, content)
-	RequireNoError(t, err)
-	err = metadata.PutBlob(name, digest)
-	RequireNoError(t, err)
+	err := s.blobs.PutBlob(digest, content)
+	RequireNoError(s.T(), err)
+	err = s.metadata.PutBlob(name, digest)
+	RequireNoError(s.T(), err)
 
-	t.Run("Known blob returns no error", func(t *testing.T) {
-		_, err := service.StatBlob(name, digest.String())
+	s.T().Run("Known blob returns no error", func(t *testing.T) {
+		_, err := s.repository.StatBlob(name, digest.String())
 		AssertNoError(t, err)
 	})
 
-	t.Run("Unknown blob returns ErrBlobUnknown", func(t *testing.T) {
-		_, err := service.StatBlob("a", "b")
+	s.T().Run("Unknown blob returns ErrBlobUnknown", func(t *testing.T) {
+		_, err := s.repository.StatBlob("a", "b")
 		AssertErrorIs(t, err, repository.ErrBlobUnknown)
 	})
 
-	t.Run("Known blob in unknown repository returns ErrBlobUnknown", func(t *testing.T) {
-		_, err := service.StatBlob("fake/repository", digest.String())
+	s.T().Run("Known blob in unknown repository returns ErrBlobUnknown", func(t *testing.T) {
+		_, err := s.repository.StatBlob("fake/repository", digest.String())
 		AssertErrorIs(t, err, repository.ErrBlobUnknown)
 	})
 }
 
-func TestGetBlob(t *testing.T) {
-	service, metadata, blobs := newTestRepository()
-
+func (s *Suite) TestGetBlob() {
 	name := RandomName()
 	digest, content := RandomBlob(32)
 
-	err := blobs.PutBlob(digest, content)
-	RequireNoError(t, err)
-	err = metadata.PutBlob(name, digest)
-	RequireNoError(t, err)
+	err := s.blobs.PutBlob(digest, content)
+	RequireNoError(s.T(), err)
+	err = s.metadata.PutBlob(name, digest)
+	RequireNoError(s.T(), err)
 
-	t.Run("Known blob returns content and no error", func(t *testing.T) {
-		r, err := service.GetBlob(name, digest.String())
+	s.T().Run("Known blob returns content and no error", func(t *testing.T) {
+		r, err := s.repository.GetBlob(name, digest.String())
 		AssertNoError(t, err)
 		data, err := io.ReadAll(r)
 		AssertNoError(t, err)
 		AssertSlicesEqual(t, data, content)
 	})
 
-	t.Run("Unknown blob returns no content and ErrBlobUnknown", func(t *testing.T) {
-		_, err := service.GetBlob("fake/repository", "blabla")
+	s.T().Run("Unknown blob returns no content and ErrBlobUnknown", func(t *testing.T) {
+		_, err := s.repository.GetBlob("fake/repository", "blabla")
 		AssertErrorIs(t, err, repository.ErrBlobUnknown)
 	})
 
-	t.Run("Known blob on unknown repository still returns no content and ErrBlobUnknown", func(t *testing.T) {
-		_, err := service.GetBlob("fake/repository", digest.String())
+	s.T().Run("Known blob on unknown repository still returns no content and ErrBlobUnknown", func(t *testing.T) {
+		_, err := s.repository.GetBlob("fake/repository", digest.String())
 		AssertErrorIs(t, err, repository.ErrBlobUnknown)
 	})
 }
 
-func TestDeleteBlob(t *testing.T) {
-	service, metadata, blobs := newTestRepository()
-
+func (s *Suite) TestDeleteBlob() {
 	name := RandomName()
 	digest, content := RandomBlob(32)
 
-	t.Run("A deleted blob is not retrievable", func(t *testing.T) {
-		err := blobs.PutBlob(digest, content)
+	s.T().Run("A deleted blob is not retrievable", func(t *testing.T) {
+		err := s.blobs.PutBlob(digest, content)
 		RequireNoError(t, err)
-		err = metadata.PutBlob(name, digest)
-		RequireNoError(t, err)
-
-		_, err = service.GetBlob(name, digest.String())
+		err = s.metadata.PutBlob(name, digest)
 		RequireNoError(t, err)
 
-		err = service.DeleteBlob(name, digest.String())
+		_, err = s.repository.GetBlob(name, digest.String())
 		RequireNoError(t, err)
 
-		_, err = service.GetBlob(name, digest.String())
+		err = s.repository.DeleteBlob(name, digest.String())
+		RequireNoError(t, err)
+
+		_, err = s.repository.GetBlob(name, digest.String())
 		AssertErrorIs(t, err, repository.ErrBlobUnknown)
 	})
 }
--- repository/service_test.go
+++ repository/service_test.go
@@ -1,15 +1,38 @@
 package repository_test
 
 import (
+	"testing"
+
 	"github.com/robinkb/cascade-registry/repository"
 	"github.com/robinkb/cascade-registry/store"
 	"github.com/robinkb/cascade-registry/store/inmemory"
+	"github.com/stretchr/testify/suite"
 )
 
-func newTestRepository() (repository.RepositoryService, store.Metadata, store.Blobs) {
-	metadata := inmemory.NewMetadataStore()
-	blobs := inmemory.NewBlobStore()
-	service := repository.NewRepositoryService(metadata, blobs)
+type Constructor func() (repository.RepositoryService, store.Metadata, store.Blobs)
 
-	return service, metadata, blobs
+type Suite struct {
+	suite.Suite
+
+	Constructor Constructor
+
+	repository repository.RepositoryService
+	metadata   store.Metadata
+	blobs      store.Blobs
+}
+
+func (s *Suite) SetupSuite() {
+	s.repository, s.metadata, s.blobs = s.Constructor()
+}
+
+func TestWithInMemory(t *testing.T) {
+	suite.Run(t, &Suite{
+		Constructor: func() (repository.RepositoryService, store.Metadata, store.Blobs) {
+			metadata := inmemory.NewMetadataStore()
+			blobs := inmemory.NewBlobStore()
+			repository := repository.NewRepositoryService(metadata, blobs)
+
+			return repository, metadata, blobs
+		},
+	})
 }
