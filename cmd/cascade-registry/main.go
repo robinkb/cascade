724e65dd10a07432ef058ee746bd217d0c49b30e
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2025-05-27 19:08:36 +0200

    Wire up registry to persist data

--- cmd/cascade-registry/main.go
+++ cmd/cascade-registry/main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/robinkb/cascade-registry"
 	"github.com/robinkb/cascade-registry/server"
-	"github.com/robinkb/cascade-registry/store/inmemory"
+	"github.com/robinkb/cascade-registry/store/boltdb"
+	"github.com/robinkb/cascade-registry/store/fs"
 )
 
 func main() {
-	metadata := inmemory.NewMetadataStore()
-	blobs := inmemory.NewBlobStore()
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("failed to get working directory: %s", err)
+	}
+
+	metadata := boltdb.NewMetadataStore(path)
+	blobs := fs.NewBlobStore(path)
 	service := cascade.NewRegistryService(metadata, blobs)
 	server := server.New(service)
 	log.Fatal(http.ListenAndServe(":5000", logger(server)))
