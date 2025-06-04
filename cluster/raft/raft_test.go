fb6adcb9016e0c9cfbd76af3cbd96970510ec212
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2025-06-04 21:43:14 +0200

    Clean up test code

--- cluster/raft/raft_test.go
+++ cluster/raft/raft_test.go
@@ -6,78 +6,81 @@ import (
 	"testing"
 	"time"
 
+	"github.com/robinkb/cascade-registry/cluster"
 	"github.com/robinkb/cascade-registry/store/inmemory"
 	. "github.com/robinkb/cascade-registry/testing"
 )
+
+var (
+	localhost = net.ParseIP("127.0.0.1")
+)
+
+func newTestCluster(n int) []cluster.Node {
+	nodes := make([]cluster.Node, n)
+	peers := make([]Peer, n)
+	for i := range peers {
+		peers[i] = Peer{
+			ID: rand.Uint64(),
+			Addr: net.TCPAddr{
+				IP:   localhost,
+				Port: randomPort(),
+			},
+		}
+	}
+
+	for i := range nodes {
+		nodes[i] = NewRaftNode(
+			peers[i].ID,
+			&peers[i].Addr,
+			peers,
+			inmemory.NewMetadataStore(),
+		)
+	}
+
+	return nodes
+}
 
 func randomPort() int {
 	return rand.IntN(30000) + 1024
 }
 
 func TestRaftClusterFormation(t *testing.T) {
-	localhost := net.ParseIP("127.0.0.1")
-	peers := []Peer{
-		{ID: 1, Addr: net.TCPAddr{IP: localhost, Port: randomPort()}},
-		{ID: 2, Addr: net.TCPAddr{IP: localhost, Port: randomPort()}},
-		{ID: 3, Addr: net.TCPAddr{IP: localhost, Port: randomPort()}},
+	nodes := newTestCluster(3)
+
+	for _, n := range nodes {
+		AssertEqual(t, n.ClusterStatus().Clustered, false)
+		n.Start()
 	}
-	n1 := NewRaftNode(1, &peers[0].Addr, peers, inmemory.NewMetadataStore())
-	n2 := NewRaftNode(2, &peers[1].Addr, peers, inmemory.NewMetadataStore())
-	n3 := NewRaftNode(3, &peers[2].Addr, peers, inmemory.NewMetadataStore())
-
-	AssertEqual(t, n1.ClusterStatus().Clustered, false)
-	AssertEqual(t, n2.ClusterStatus().Clustered, false)
-	AssertEqual(t, n3.ClusterStatus().Clustered, false)
-
-	n1.Start()
-	n2.Start()
-	n3.Start()
 
 	time.Sleep(150 * time.Millisecond)
 
-	AssertEqual(t, n1.ClusterStatus().Clustered, true)
-	AssertEqual(t, n2.ClusterStatus().Clustered, true)
-	AssertEqual(t, n3.ClusterStatus().Clustered, true)
+	for _, n := range nodes {
+		AssertEqual(t, n.ClusterStatus().Clustered, true)
+	}
 }
 
 func TestRaftClusterReplication(t *testing.T) {
-	peers := []Peer{
-		{ID: 1, Addr: net.TCPAddr{Port: randomPort()}},
-		{ID: 2, Addr: net.TCPAddr{Port: randomPort()}},
-		{ID: 3, Addr: net.TCPAddr{Port: randomPort()}},
+	nodes := newTestCluster(3)
+	for _, n := range nodes {
+		n.Start()
 	}
-	ms1 := inmemory.NewMetadataStore()
-	n1 := NewRaftNode(1, &peers[0].Addr, peers, ms1)
-	ms2 := inmemory.NewMetadataStore()
-	n2 := NewRaftNode(2, &peers[1].Addr, peers, ms2)
-	ms3 := inmemory.NewMetadataStore()
-	n3 := NewRaftNode(3, &peers[2].Addr, peers, ms3)
-
-	n1.Start()
-	n2.Start()
-	n3.Start()
 
 	// Allow some time for cluster formation.
 	time.Sleep(150 * time.Millisecond)
 
 	name, tag, digest := RandomName(), RandomVersion(), RandomDigest()
-	ms1.CreateRepository(name)
-	ms2.CreateRepository(name)
-	ms3.CreateRepository(name)
+	for _, n := range nodes {
+		n.(*node).Metadata.CreateRepository(name)
+	}
 
-	n1.PutTag(name, tag, digest)
+	err := nodes[0].PutTag(name, tag, digest)
+	AssertNoError(t, err)
 
 	time.Sleep(1 * time.Millisecond)
 
-	got, err := n1.GetTag(name, tag)
-	AssertNoError(t, err)
-	AssertEqual(t, got, digest)
-
-	got, err = n2.GetTag(name, tag)
-	AssertNoError(t, err)
-	AssertEqual(t, got, digest)
-
-	got, err = n3.GetTag(name, tag)
-	AssertNoError(t, err)
-	AssertEqual(t, got, digest)
+	for _, n := range nodes {
+		got, err := n.GetTag(name, tag)
+		AssertNoError(t, err)
+		AssertEqual(t, got, digest)
+	}
 }
