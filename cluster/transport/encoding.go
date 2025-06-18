32d20e25e23862fff3e69c0dd417c9444adc2f2d
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2025-06-18 08:27:08 +0200

    Initial implementation of streaming encoding

--- cluster/transport/encoding.go
+++ cluster/transport/encoding.go
@@ -3,20 +3,23 @@ package transport
 import (
 	"bytes"
 	"encoding/binary"
+	"io"
 	"log"
 )
 
 type (
-	BufferedEncoder interface {
+	Encoder interface {
 		Encode(id MessageType, data []byte) ([]byte, error)
+		EncodeStream(id MessageType, r io.Reader) (io.Reader, error)
 	}
 
-	BufferedDecoder interface {
+	Decoder interface {
 		Decode(data []byte) (MessageType, []byte, error)
+		DecodeStream(r io.Reader) (MessageType, io.Reader, error)
 	}
 )
 
-func NewBufferedEncoder() BufferedEncoder {
+func NewBufferedEncoder() Encoder {
 	return &bufferedEncoder{
 		buf: bytes.NewBuffer(make([]byte, 0, 1024)),
 	}
@@ -35,13 +38,48 @@ func (e *bufferedEncoder) Encode(id MessageType, data []byte) ([]byte, error) {
 	}
 
 	e.buf.Reset()
-	e.buf.Write(header.bytes())
+	e.buf.Write(header.Bytes())
 	e.buf.Write(data)
 
 	return e.buf.Bytes(), nil
 }
 
-func NewBufferedDecoder() BufferedDecoder {
+func (e *bufferedEncoder) EncodeStream(id MessageType, r io.Reader) (io.Reader, error) {
+	attributes := attributes{
+		stream: true,
+	}
+
+	e.buf.Reset()
+	n, err := io.Copy(e.buf, r)
+	if err != nil {
+		return nil, err
+	}
+
+	header := header{
+		magicNumber: headerMagicNumber,
+		attributes:  attributes.Byte(),
+		messageType: uint16(id),
+		length:      uint32(n),
+	}
+
+	msg := new(bytes.Buffer)
+	msg.Write(header.Bytes())
+	msg.Write(e.buf.Bytes())
+
+	return &streamEncoder{id, r, msg}, nil
+}
+
+type streamEncoder struct {
+	mtype MessageType
+	r     io.Reader
+	buf   *bytes.Buffer
+}
+
+func (e *streamEncoder) Read(p []byte) (int, error) {
+	return e.buf.Read(p)
+}
+
+func NewBufferedDecoder() Decoder {
 	return &bufferedDecoder{}
 }
 
@@ -51,6 +89,33 @@ type bufferedDecoder struct {
 func (d *bufferedDecoder) Decode(data []byte) (MessageType, []byte, error) {
 	header := parseHeader(data[:headerSize])
 	return MessageType(header.messageType), data[headerSize:], nil
+}
+
+func (d *bufferedDecoder) DecodeStream(r io.Reader) (MessageType, io.Reader, error) {
+	buf := make([]byte, headerSize)
+	_, err := io.ReadFull(r, buf)
+	if err != nil {
+		panic(err)
+	}
+	header := parseHeader(buf)
+	data := make([]byte, 0, header.length)
+	io.ReadFull(r, data)
+
+	return MessageType(header.messageType), &streamDecoder{
+		mtype: MessageType(header.messageType),
+		r:     r,
+		buf:   bytes.NewBuffer(data),
+	}, nil
+}
+
+type streamDecoder struct {
+	mtype MessageType
+	r     io.Reader
+	buf   *bytes.Buffer
+}
+
+func (d *streamDecoder) Read(p []byte) (int, error) {
+	return d.r.Read(p)
 }
 
 const (
@@ -78,11 +143,35 @@ type header struct {
 	length      uint32
 }
 
-func (h header) bytes() []byte {
+func (h header) Attributes() attributes {
+	return parseAttributes(h.attributes)
+}
+
+func (h header) Bytes() []byte {
 	buf := make([]byte, headerSize)
 	buf[0] = h.magicNumber
 	buf[1] = h.attributes
 	binary.LittleEndian.PutUint16(buf[2:4], h.messageType)
 	binary.LittleEndian.PutUint32(buf[4:8], h.length)
 	return buf
+}
+
+func parseAttributes(b byte) attributes {
+	var a attributes
+	if b>>7 == 1 {
+		a.stream = true
+	}
+	return a
+}
+
+type attributes struct {
+	stream bool
+}
+
+func (a attributes) Byte() byte {
+	var b byte
+	if a.stream {
+		b += 0b10000000
+	}
+	return b
 }
--- cluster/transport/encoding_test.go
+++ cluster/transport/encoding_test.go
@@ -1,13 +1,15 @@
 package transport
 
 import (
+	"bytes"
+	"io"
 	"math/rand/v2"
 	"testing"
 
 	. "github.com/robinkb/cascade-registry/testing"
 )
 
-func TestEncodeDecode(t *testing.T) {
+func TestEncodeDecodeMessage(t *testing.T) {
 	encoder := NewBufferedEncoder()
 	decoder := NewBufferedDecoder()
 
@@ -19,6 +21,26 @@ func TestEncodeDecode(t *testing.T) {
 	gotID, gotData, err := decoder.Decode(encoded)
 	RequireNoError(t, err)
 	AssertEqual(t, gotID, wantID)
+	AssertSlicesEqual(t, gotData, wantData)
+}
+
+func TestEncodeDecodeStream(t *testing.T) {
+	encoder := NewBufferedEncoder()
+	decoder := NewBufferedDecoder()
+
+	wantID := MessageType(rand.UintN(1000))
+	wantData := RandomContents(128)
+	wantBuf := bytes.NewBuffer(wantData)
+
+	encoded, err := encoder.EncodeStream(wantID, wantBuf)
+	RequireNoError(t, err)
+
+	gotID, decoded, err := decoder.DecodeStream(encoded)
+	RequireNoError(t, err)
+	AssertEqual(t, gotID, wantID)
+
+	gotData, err := io.ReadAll(decoded)
+	RequireNoError(t, err)
 	AssertSlicesEqual(t, gotData, wantData)
 }
 
@@ -35,4 +57,26 @@ func TestEncodingDecodingDoesNotAllocate(t *testing.T) {
 	})
 
 	AssertEqual(t, allocs, 0)
+}
+
+func TestAttributesEncodeDecode(t *testing.T) {
+	t.Run("Encoding stream attribute", func(t *testing.T) {
+		want := byte(0b10000000)
+
+		attr := attributes{
+			stream: true,
+		}
+
+		got := attr.Byte()
+		AssertEqual(t, got, want)
+	})
+
+	t.Run("Decoding stream attribute", func(t *testing.T) {
+		attr := byte(0b10000000)
+		want := attributes{
+			stream: true,
+		}
+		got := parseAttributes(attr)
+		AssertEqual(t, got, want)
+	})
 }
