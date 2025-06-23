608a09b68816e745bd69a0ec2547088c294b2d38
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2025-06-23 22:44:56 +0200

    Refine and document Proposer interface (#63)

--- cluster/raft/proposer.go
+++ cluster/raft/proposer.go
@@ -13,17 +13,34 @@ import (
 )
 
 type (
-	Operation interface {
+	// Proposal defines an interface for creating proposal types.
+	// Implementers must return a stored random ID, but can include additional attributes.
+	Proposal interface {
 		ID() uint64
 	}
 
-	HandlerFunc func(op Operation) error
+	// HandlerFunc is a function that handles committing a proposal type.
+	// They typically type assert the given proposal into its concrete type,
+	// and process the payload by committing it to the state machine.
+	HandlerFunc func(p Proposal) error
 
+	// Proposer encapsulates making proposals to the Raft log,
+	// and handles committing proposals once they are accepted.
 	Proposer interface {
-		Propose(o Operation) error
-		// Consumers must call Handle() to register a function that processes operations.
-		Handle(op Operation, f HandlerFunc)
-		Handler(data []byte)
+		// Consumers must call Handle() to register a function that commits
+		// proposals of a certain concrete type.
+		Handle(p Proposal, f HandlerFunc)
+		// Propose makes a proposal to the Raft log.
+		//
+		// Propose panics if a HandlerFunc has not been registered
+		// for the given proposal type using Handle.
+		Propose(p Proposal) error
+		// Commit decodes the payload into a registered proposal type,
+		// and calls its HandlerFunc.
+		//
+		// Commit panics if a HandlerFunc has not been registered
+		// for the given proposal type using Handle.
+		Commit(data []byte)
 	}
 )
 
@@ -31,7 +48,7 @@ func NewProposer(node raft.Node) *proposer {
 	return &proposer{
 		raft:         node,
 		handlerFuncs: make(map[reflect.Type]HandlerFunc),
-		errs: opErrors{
+		errs: errMap{
 			errs: make(map[uint64]chan error),
 		},
 	}
@@ -40,16 +57,25 @@ func NewProposer(node raft.Node) *proposer {
 type proposer struct {
 	raft         raft.Node
 	handlerFuncs map[reflect.Type]HandlerFunc
-	errs         opErrors
+	errs         errMap
 }
 
-func (p *proposer) Propose(op Operation) error {
+func (p *proposer) Handle(proposal Proposal, f HandlerFunc) {
+	t := reflect.TypeOf(proposal)
+	if _, ok := p.handlerFuncs[t]; ok {
+		log.Fatalf("proposal type already registered: %T", proposal)
+	}
+	p.handlerFuncs[reflect.TypeOf(proposal)] = f
+	gob.Register(proposal)
+}
+
+func (p *proposer) Propose(proposal Proposal) error {
 	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(&op); err != nil {
-		log.Panicf("failed to encode operation: %s\n", err)
+	if err := gob.NewEncoder(&buf).Encode(&proposal); err != nil {
+		log.Panicf("failed to encode proposal: %s\n", err)
 	}
 
-	errC := p.errs.create(op.ID())
+	errC := p.errs.create(proposal.ID())
 	// Propose blocks until accepted by the cluster.
 	// TODO: Find out how to retry proposals.
 	err := p.raft.Propose(context.TODO(), buf.Bytes())
@@ -65,59 +91,51 @@ func (p *proposer) Propose(op Operation) error {
 	}
 }
 
-func (p *proposer) Handle(op Operation, f HandlerFunc) {
-	t := reflect.TypeOf(op)
-	if _, ok := p.handlerFuncs[t]; ok {
-		log.Fatalf("operation already registered: %T", op)
-	}
-	p.handlerFuncs[reflect.TypeOf(op)] = f
-	gob.Register(op)
-}
-
-func (p *proposer) Handler(data []byte) {
+func (p *proposer) Commit(data []byte) {
 	buf := bytes.NewBuffer(data)
 
-	var op Operation
-	err := gob.NewDecoder(buf).Decode(&op)
+	var proposal Proposal
+	err := gob.NewDecoder(buf).Decode(&proposal)
 	if err != nil {
-		log.Panicf("unable to decode as operation: %s", err)
+		log.Panicf("unable to decode as proposal: %s", err)
 	}
 
-	f, ok := p.handlerFuncs[reflect.TypeOf(op)]
+	f, ok := p.handlerFuncs[reflect.TypeOf(proposal)]
 	if !ok {
-		log.Panicf("unknown operation received: %T", op)
+		log.Panicf("unknown proposal type received: %T", proposal)
 	}
-	err = f(op)
 
-	errC, ok := p.errs.get(op.ID())
+	err = f(proposal)
+
+	errC, ok := p.errs.get(proposal.ID())
 	if ok {
 		if err != nil {
 			errC <- err
 		}
-		p.errs.delete(op.ID())
+		p.errs.delete(proposal.ID())
 	}
 }
 
-type opErrors struct {
+type errMap struct {
 	mu   sync.RWMutex
 	errs map[uint64]chan error
 }
 
-func (e *opErrors) create(id uint64) chan error {
+func (e *errMap) create(id uint64) chan error {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	e.errs[id] = make(chan error)
 	return e.errs[id]
 }
 
-func (e *opErrors) get(id uint64) (chan error, bool) {
+func (e *errMap) get(id uint64) (chan error, bool) {
 	e.mu.RLock()
 	defer e.mu.RUnlock()
 	err, ok := e.errs[id]
 	return err, ok
 }
 
-func (e *opErrors) delete(id uint64) {
+func (e *errMap) delete(id uint64) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	close(e.errs[id])
