8b590ea492a60bb57cfc394bfff0e4382b4796b6
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2026-04-06 14:21:44 +0200

    Amend FakeProposer to count proposals

--- cluster/fake_proposer.go
+++ cluster/fake_proposer.go
@@ -6,32 +6,37 @@ import (
 	"math/rand/v2"
 )
 
-// NewFakeProposer returns a Proposer that handles proposals without actually
-// submitting them to a cluster.
-func NewFakeProposer() Proposer {
-	return &fakeProposer{
+// NewFakeProposer returns a new FakeProposer.
+func NewFakeProposer() *FakeProposer {
+	return &FakeProposer{
 		handlerFuncs: make(map[Operation]HandlerFunc),
 	}
 }
 
-type fakeProposer struct {
+// FakeProposer handles proposals without actually submitting them to a cluster.
+type FakeProposer struct {
 	handlerFuncs map[Operation]HandlerFunc
+
+	// Proposals is incremented for every submitted proposal.
+	Proposals int64
 }
 
-func (p *fakeProposer) Handle(t Operation, f HandlerFunc) {
+func (p *FakeProposer) Handle(t Operation, f HandlerFunc) {
 	if _, ok := p.handlerFuncs[t]; ok {
 		log.Fatalf("proposal type already registered: %s", t)
 	}
 	p.handlerFuncs[t] = f
 }
 
-func (p *fakeProposer) Propose(req Request) Response {
+func (p *FakeProposer) Propose(req Request) Response {
 	req.ID = rand.Uint64()
 
 	f, ok := p.handlerFuncs[req.Op]
 	if !ok {
 		panic(fmt.Sprintf("unknown proposal type received: %s", req.Op))
 	}
+
+	p.Proposals++
 
 	return f(req)
 }
