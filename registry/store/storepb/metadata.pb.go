d473bd9fa62fb5e2ca8891db635c546e3c15297f
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2026-05-07 21:11:39 +0200

    Add Taskfile for basic automation

--- registry/store/storepb/metadata.pb.go
+++ registry/store/storepb/metadata.pb.go
@@ -7,12 +7,11 @@
 package storepb
 
 import (
-	reflect "reflect"
-	unsafe "unsafe"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	reflect "reflect"
+	unsafe "unsafe"
 )
 
 const (
